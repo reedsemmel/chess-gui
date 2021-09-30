ebfb9f0ff6c3901913762a4ac3cad55e4902f9e1
Reed Semmel <reed@eperm.dev>
2021-09-30 01:48:04 -0400

    interactive: docs and fixes from code review

--- src/interactive_board.py
+++ src/interactive_board.py
@@ -6,112 +6,131 @@ Description:
     Classes and widgets for the interactive game board.
 """
 
+
+from enum import Enum, unique
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QGridLayout, QLabel, QWidget
 
-"""An interactive chess board widget.
+@unique
+class Piece(Enum):
+    NONE = 0
+    WP = 1
+    WR = 2
+    WN = 3
+    WB = 4
+    WQ = 5
+    WK = 6
+    BP = 7
+    BR = 8
+    BN = 9
+    BB = 10
+    BQ = 11
+    BK = 12
 
-This widget will draw a basic chess board, though it is useless by itself. There
-is no underlying engine or logic. It just draws the pieces and lets you click on
-the tiles, which triggers a function. This must be interfaced with a backend
-logic to become complete.
-This widget imposes no restrictions on geometry. The parent MUST ensure that
-this widget remains a square exactly pixel by pixel, otherwise the drawing will
-be off.
-"""
 class InteractiveBoard(QWidget):
+    """An interactive chess board widget.
+
+    This widget will draw a basic chess board, though it is useless by itself.
+    There is no underlying engine or logic. It just draws the pieces and lets
+    you click on the tiles, which triggers a function. This must be interfaced
+    with a backend logic to become complete.
+    This widget imposes no restrictions on geometry. The parent MUST ensure that
+    this widget remains a square exactly pixel by pixel, otherwise the drawing
+    will be off.
+    """
+
     def __init__(self) -> None:
         super().__init__()
-        # Load up all the assets we need
 
+        # Load up all the assets we need
         # TODO: Fix these piece images to be exactly square
         self.piece_img = {
-            'wr': QtGui.QPixmap("assets/wr.png"),
-            'wb': QtGui.QPixmap("assets/wb.png"),
-            'wn': QtGui.QPixmap("assets/wn.png"),
-            'wq': QtGui.QPixmap("assets/wq.png"),
-            'wk': QtGui.QPixmap("assets/wk.png"),
-            'wp': QtGui.QPixmap("assets/wp.png"),
-            'br': QtGui.QPixmap("assets/br.png"),
-            'bb': QtGui.QPixmap("assets/bb.png"),
-            'bn': QtGui.QPixmap("assets/bn.png"),
-            'bq': QtGui.QPixmap("assets/bq.png"),
-            'bk': QtGui.QPixmap("assets/bk.png"),
-            'bp': QtGui.QPixmap("assets/bp.png"),
-            'none': QtGui.QPixmap("assets/blank.png")
+            Piece.NONE: QtGui.QPixmap("assets/blank.png"),
+            Piece.WR: QtGui.QPixmap("assets/wr.png"),
+            Piece.WB: QtGui.QPixmap("assets/wb.png"),
+            Piece.WN: QtGui.QPixmap("assets/wn.png"),
+            Piece.WQ: QtGui.QPixmap("assets/wq.png"),
+            Piece.WK: QtGui.QPixmap("assets/wk.png"),
+            Piece.WP: QtGui.QPixmap("assets/wp.png"),
+            Piece.BR: QtGui.QPixmap("assets/br.png"),
+            Piece.BB: QtGui.QPixmap("assets/bb.png"),
+            Piece.BN: QtGui.QPixmap("assets/bn.png"),
+            Piece.BQ: QtGui.QPixmap("assets/bq.png"),
+            Piece.BK: QtGui.QPixmap("assets/bk.png"),
+            Piece.BP: QtGui.QPixmap("assets/bp.png"),
         }
         
         # Set up an 8 by 8 grid
         layout = QGridLayout(self)
         layout.setSpacing(0)
 
-        self.tileGrid = []
-
-        # Create the 64 tiles and put them in a grid layout
+        self.tileGrid = [[self.ChessTile(i, j) for i in range(8)] for j in range(8)]
         for i in range(8):
-            self.tileGrid.append([])
             for j in range(8):
-                self.tileGrid[i].append(ChessTile(i, j))
                 layout.addWidget(self.tileGrid[i][j], i, j)
+
         self.setLayout(layout)
-        print(self.tileGrid)
     
-    #Called by ChessTile widgets and passes their position to this function
-    def handleClick(self, x, y) -> None:
+    # Called by ChessTile widgets and passes their position to this function
+    def handleClick(self, x: int, y: int) -> None:
+        """Handles a click from the user
+        
+        Called by a tile once it is clicked. The tile provides the arguments
+        x and y from its position on the grid
+        """
         # Dummy implementation for now. Just for testing
         print("Tile", x, y, "clicked")
     
     # Replaces the piece on (x,y) with the piece name from the piece_img array
-    def drawPiece(self, piece: str, x: int, y: int) -> None:
-        self.tileGrid[x][y].label.setPixmap(self.piece_img[piece])
-"""A basic chess tile widget
-
-A simple widget which holds nothing but a piece image and forwards clicks to
-the parent.
-"""
-class ChessTile(QWidget):
-    # TODO: add a resize event to redraw the pixmaps at a corrected size
-    def __init__(self, x, y) -> None:
-        super().__init__()
-        self.x = x
-        self.y = y
-        # Holds the image we display
-        self.label = QLabel(self)
-
-        # TODO: pick less ugly colors
-        # Chess is played on a checkered board. Adding the x and y index is an
-        # easy way to see which we are on
-        if ((self.x + self.y) % 2):
-            self.setStyleSheet("background-color: #573D11")
+    def drawPiece(self, piece: Piece, x: int, y: int) -> None:
+        """Draws and replaced a piece on the board"""
+        if 0 <= x <= 7 and 0 <= y <= 7:
+            self.tileGrid[y][x].setImage(self.piece_img[piece])
         else:
-            self.setStyleSheet("background-color: #DCB167")
+            print(f"Invalid index {x} {y}")
+    
+    class ChessTile(QWidget):
+        """A basic chess tile widget
 
+        A simple widget which holds nothing but a piece image and forwards
+        clicks to the parent.
+        """
+        # TODO: add a resize event to redraw the pixmaps at a corrected size
+        def __init__(self, x: int, y: int) -> None:
+            super().__init__()
+            self.x = x
+            self.y = y
+            # Holds the image we display
+            self.label = QLabel(self)
 
-    # Forward clicks from each child for the main widget to handle
-    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
-        self.parentWidget().handleClick(self.x, self.y)
+            # TODO: pick less ugly colors
+            # Chess is played on a checkered board. Adding the x and y index is an
+            # easy way to see which we are on
+            if ((self.x + self.y) % 2):
+                self.setStyleSheet("background-color: #573D11;")
+            else:
+                self.setStyleSheet("background-color: #DCB167;")
+
+        def setImage(self, piece: QtGui.QPixmap):
+            self.label.setPixmap(piece)
+
+        def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+            self.parentWidget().handleClick(self.x, self.y)
 
 # Basic testing code
+
 if (__name__ == "__main__"):
     import sys
     from PyQt5.QtWidgets import QApplication
     app = QApplication(sys.argv)
     widget = InteractiveBoard()
 
-    defaultBoard = [
-        ['br', 'bn', 'bb', 'bq', 'bk', 'bb', 'bn', 'br'],
-        ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
-        ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-        ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-        ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-        ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none'],
-        ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
-        ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr'],
-    ]
+    defaultBoard = [[Piece.NONE for _ in range(8)] for _ in range(8)]
+    defaultBoard[2][3] = Piece.WR
     for i in range(8):
         for j in range(8):
             widget.drawPiece(defaultBoard[i][j], i, j)
 
-    widget.setGeometry(0, 0, 800, 800)
+    widget.setGeometry(0, 0, 1000, 1000)
     widget.show()
     app.exec_()
