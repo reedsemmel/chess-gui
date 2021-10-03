1a43737f64a683978369dcbe14211142958b3272
Alishan Bhayani <58956869+M9909-AB@users.noreply.github.com>
2021-10-02 21:10:09 -0500

    Added screens and integrates interactive board #5

--- src/app_main.py
+++ src/app_main.py
@@ -10,7 +10,9 @@ import sys
 
 from enum import Enum
 from PyQt5.QtGui import QCloseEvent, QIcon
-from PyQt5.QtWidgets import QAction, QApplication, QMainWindow, QMenu, QMessageBox
+from PyQt5.QtWidgets import QAction, QApplication, QMainWindow, QMenu, QMessageBox, QWidget
+
+from screens import EndGameScreen, GameScreen, MainMenuScreen
 
 class GameWindow(QMainWindow):
     """Main Window for the game that will house all the GUI elements."""
@@ -25,37 +27,53 @@ class GameWindow(QMainWindow):
             SAVING = 5
             END_OF_GAME = 6
 
-        def __init__(self) -> None:
+        def __init__(self, parent: QMainWindow) -> None:
             self.game_logic = None
-            self.game_ui = None
+            self.game_ui: QWidget = None
+            self.parent: QMainWindow = parent
             self.state: self.State = self.State.PRE_LAUNCH
+
+        def end(self) -> None:
+            """Marks end of game and changes to end of game screen."""
+            if self.state == self.State.GAME:
+                self.state: self.State = self.State.LOADING
+                self.game_ui: EndGameScreen = EndGameScreen(self.parent)
+                self.state: self.State = self.State.END_OF_GAME
+                self.parent.setCentralWidget(self.game_ui)
 
         def load(self) -> None:
             """Any actions to load from a previously saved game state would want to run here."""
             if self.state in (self.State.MAIN_MENU, self.State.GAME):
-                self.state = self.State.LOADING
-                # Do loading stuff
-                self.state = self.State.GAME
+                self.state: self.State = self.State.LOADING
+                self.game_ui: GameScreen = GameScreen(self.parent)
+                self.state: self.State = self.State.GAME
+                self.parent.setCentralWidget(self.game_ui)
+
+        def play(self) -> None:
+            """Starts a new game."""
+            if self.state == self.State.MAIN_MENU:
+                self.load()
 
         def run(self) -> None:
             """Create the window and initializes all of the game componenets."""
             assert self.state == self.State.PRE_LAUNCH
-            # Do pre launch stuff
-            self.state = self.State.MAIN_MENU
+            self.game_ui: MainMenuScreen = MainMenuScreen(self.parent)
+            self.state: self.State = self.State.MAIN_MENU
+            self.parent.setCentralWidget(self.game_ui)
 
         def save(self) -> None:
             """Any actions to preserve game state would want to run here."""
             if self.state == self.State.GAME:
-                self.state = self.State.SAVING
+                self.state: self.State = self.State.SAVING
                 # Do saving stuff
-                self.state = self.State.GAME
+                self.state: self.State = self.State.GAME
 
     def __init__(self) -> None:
         super().__init__()
         self._set_window_properties()
         self._set_menu_bar()
 
-        self.game: GameWindow.Game = GameWindow.Game()
+        self.game: GameWindow.Game = GameWindow.Game(self)
 
     def closeEvent(self, event: QCloseEvent) -> None: # pylint: disable=invalid-name
         """
@@ -74,9 +92,17 @@ class GameWindow(QMainWindow):
         event.ignore()
         return None
 
+    def end_event(self) -> None:
+        """Triggers the end of a game."""
+        self.game.end()
+
     def load_event(self) -> None:
         """Triggers loading of a game."""
         self.game.load()
+
+    def play_event(self) -> None:
+        """Triggers start of a new game."""
+        self.game.play()
 
     def save_event(self) -> None:
         """Triggers saving of the game."""
@@ -85,7 +111,6 @@ class GameWindow(QMainWindow):
     def show(self) -> None:
         """Shows the window and runs the setup for the game."""
         self.game.run()
-        # self.setCentralWidget() # Assign to Main Menu here
         super().show()
 
     def _set_menu_bar(self) -> None:
@@ -96,7 +121,7 @@ class GameWindow(QMainWindow):
 
         self.quit_action: QAction = QAction(QIcon.fromTheme("application-exit"), "Quit", self)
         self.quit_action.triggered.connect(self.close)
-        
+
         self.save_action: QAction = QAction(QIcon.fromTheme("document-save"), "Save", self)
         self.save_action.setShortcut("Ctrl+S")
         self.save_action.triggered.connect(self.save_event)
---
+++ src/screens.py
@@ -0,0 +1,89 @@
+"""
+File: screens.py
+Author: Alishan Bhayani
+Date: 9/27/2021
+Description:
+    Houses the the different screen states for the application.
+"""
+
+from typing import Callable, Optional, Union
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QResizeEvent
+from PyQt5.QtWidgets import QGridLayout, QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QWidget
+
+from interactive_board import InteractiveBoard
+
+def make_button(content: str, func: Callable, parent: Union[QWidget, None] = None) -> QPushButton:
+    """Returns a button with the specified content and function."""
+    button: QPushButton = QPushButton(content, parent)
+    button.clicked.connect(func)
+    return button
+
+class MainMenuScreen(QWidget):
+    """Screen that houses main menu elements like the title, play button, etc."""
+    def __init__(self, parent: Optional['QWidget'] = None) -> None:
+        super().__init__(parent)
+        self.setLayout(QGridLayout(self))
+        self._add_elements()
+
+    def _add_elements(self):
+        """Adds all the elements unique to this screen to the layout."""
+        assert isinstance(self.layout(), QGridLayout)
+
+        # Title
+        self.layout().addWidget(QLabel("<h1>CHESS</h1>"), 0, 0, Qt.AlignTop | Qt.AlignHCenter)
+
+        # Buttons
+        buttons: QVBoxLayout = QVBoxLayout()
+
+        buttons.addWidget(self.make_button("Play", self.parent().play_event, self))
+        buttons.addWidget(self.make_button("Load", self.parent().load_event, self))
+        buttons.addWidget(self.make_button("Quit", self.parent().close, self))
+
+        self.layout().addLayout(buttons, 1, 0, Qt.AlignHCenter | Qt.AlignTop)
+
+class GameScreen(QWidget):
+    """Screen that houses the actual game elements like the chessboard and other features."""
+    class CenteredSquareContainer(QWidget):
+        """Widget wrapper that keeps its child widget square and centered on both axes."""
+        def __init__(self, child: QWidget, parent: QWidget):
+            super().__init__(parent)
+            self.setLayout(QHBoxLayout(self))
+            self.layout().addWidget(child)
+
+        def resizeEvent(self, a0: QResizeEvent) -> None: # pylint: disable=invalid-name
+            """Controls resizing to ensure the child widget stays centered and square."""
+            height, width = a0.size().height(), a0.size().width()
+            if width > height:
+                margin = round((width - height) / 2)
+                self.setContentsMargins(margin, 0, margin, 0)
+            else:
+                margin = round((height - width) / 2)
+                self.setContentsMargins(0, margin, 0, margin)
+
+    def __init__(self, parent: Optional['QWidget'] = None) -> None:
+        super().__init__(parent)
+        self.setLayout(QGridLayout(self))
+        self._add_elements()
+
+    def _add_elements(self):
+        """Adds all the elements unique to this screen to the layout."""
+        assert isinstance(self.layout(), QGridLayout)
+
+        # Title
+        self.layout().addWidget(QLabel("This is the game screen.", self), 0, 0, Qt.AlignTop
+            | Qt.AlignHCenter)
+
+        # Board
+        self.layout().addWidget(self.CenteredSquareContainer(InteractiveBoard(), self), 1, 0, 8, 1)
+
+        # Temporary End Game Button
+        self.layout().addWidget(make_button("End Game", self.parent().end_event, self), 9, 0,
+            Qt.AlignBottom | Qt.AlignHCenter)
+
+class EndGameScreen(QWidget):
+    """Screen that houses the end of game elements like the winner and other features."""
+    def __init__(self, parent: Optional['QWidget'] = None) -> None:
+        super().__init__(parent)
+        self.setLayout(QGridLayout(self))
+        self.layout().addWidget(QLabel("This is the end of game screen.", self), Qt.AlignCenter)
