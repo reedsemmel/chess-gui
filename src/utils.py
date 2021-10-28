724d883e1b20936c2f5cfcfab30bf8188a4a852c
Christopher Degawa <ccom@randomderp.com>
2021-10-28 00:02:38 -0500

    NIT: utils: add rank to the left of the infographic
    Signed-off-by: Christopher Degawa <ccom@randomderp.com>

--- src/utils.py
+++ src/utils.py
@@ -12,14 +12,14 @@ Description:
 
 Board Layout
 File a b c d e f g h  Rank
-    │r│n│b│q│k│b│n│r│ 7
-    │p│p│p│p│p│p│p│p│ 6
-    │ │ │ │ │ │ │ │ │ 5
-    │ │ │ │ │ │ │ │ │ 4
-    │ │ │ │ │ │ │ │ │ 3
-    │ │ │ │ │ │ │ │ │ 2
-    │P│P│P│P│P│P│P│P│ 1
-    │R│N│B│Q│K│B│N│R│ 0
+  8 │r│n│b│q│k│b│n│r│ 7
+  7 │p│p│p│p│p│p│p│p│ 6
+  6 │ │ │ │ │ │ │ │ │ 5
+  5 │ │ │ │ │ │ │ │ │ 4
+  4 │ │ │ │ │ │ │ │ │ 3
+  3 │ │ │ │ │ │ │ │ │ 2
+  2 │P│P│P│P│P│P│P│P│ 1
+  1 │R│N│B│Q│K│B│N│R│ 0
      0 1 2 3 4 5 6 7
 """
 
